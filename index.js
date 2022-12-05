8f36111fdbc5b2e30ede03c0e9056b7a576fa568
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-05 15:15:18 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Bobby', 'Sarah']
+const findMatching = (a,b) => a.filter(a => a.toLowerCase() === b.toLowerCase())
+
+//returns a driver if the beginning provided letters match
+const fuzzyMatch = (p, q) => p.filter(p => p.charAt(0)=== q.charAt(0))
+
+//access the data structure to check if name matches
+const matchName = (p,q) => p.filter(p => p.name === q)
